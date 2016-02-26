c0eae5e637909a15838bb07dab210260a55b071b
Ivan Santos <vanil90@gmail.com>
2016-02-26 09:27:37 +0000

    Initial commit

---
+++ src/main/java/pt/tecnico/mydrive/Main.java
@@ -0,0 +1,25 @@
+package pt.tecnico.mydrive;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.File;
+
+import org.jdom2.Document;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import pt.ist.fenixframework.Atomic;
+import pt.ist.fenixframework.FenixFramework;
+
+public class Main {
+    static final Logger log = LogManager.getRootLogger();
+
+    public static void main(String[] args) throws IOException {
+        log.info("Hello World!");
+    }
+}
