da7d69c6b81451db147bf5a58e0e94aabd238940
Ivan Santos <vanil90@gmail.com>
2016-03-10 13:37:09 +0000

    Created simple Main class

--- src/main/java/pt/tecnico/myDrive/Main.java
+++ src/main/java/pt/tecnico/myDrive/Main.java
@@ -1,25 +1,28 @@
 package pt.tecnico.myDrive;
 
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.io.PrintStream;
-import java.io.File;
-
-import org.jdom2.Document;
-import org.jdom2.JDOMException;
-import org.jdom2.input.SAXBuilder;
-import org.jdom2.output.Format;
-import org.jdom2.output.XMLOutputter;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
 import pt.ist.fenixframework.Atomic;
 import pt.ist.fenixframework.FenixFramework;
+
+import java.io.IOException;
 
 public class Main {
     static final Logger log = LogManager.getRootLogger();
 
     public static void main(String[] args) throws IOException {
         log.info("Hello World!");
+        try {
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            FenixFramework.shutdown();
+        }
+    }
+
+    @Atomic
+    public static void setup() {
+
     }
 }
