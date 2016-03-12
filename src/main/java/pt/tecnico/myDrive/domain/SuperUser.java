1a7bd4a34ede1eb21b17f7da795f08e54558d5e6
Ivan Santos <vanil90@gmail.com>
2016-03-12 18:05:48 +0000

    Simple SuperUser implementation
    Implemented very simple SuperUser class.

--- src/main/java/pt/tecnico/myDrive/domain/SuperUser.java
+++ src/main/java/pt/tecnico/myDrive/domain/SuperUser.java
@@ -1,9 +1,19 @@
 package pt.tecnico.myDrive.domain;
 
 public class SuperUser extends SuperUser_Base {
-    
-    public SuperUser() {
-        super();
+    SuperUser root;
+
+    private SuperUser() {
+        setUsername("root");
+        setName("root");
+        setPassword("***");
+        setUmask("rwxdr-x-");
+        setHome("/home/root");
     }
-    
+
+    public SuperUser getInstance() {
+        if (root == null)
+            return new SuperUser();
+        return root;
+    }
 }
