6d086da551ed2103060ea99dff9d7368410f8975
RenatoL <lourinho.renato@gmail.com>
2016-03-13 18:37:45 +0000

    Added basic Directory implementation: Constructors, addFile, removeFile

--- src/main/java/pt/tecnico/myDrive/domain/Dir.java
+++ src/main/java/pt/tecnico/myDrive/domain/Dir.java
@@ -1,9 +1,51 @@
-package pt.tecnico.myDrive.domain;
+package pt.tecnico
+
+import pt.tecnico.myDrive.exception.FileAlreadyExistsException;
+
 
 public class Dir extends Dir_Base {
     
-    public Dir() {
-        super();
+	
+    public Dir(){
+		super();
+		init("/", "root", this, "rwxd----");
     }
-    
+	
+	public Dir(String name, String user, Dir directory, String permissions){
+		super();
+		init(name, user, directory, permissions);
+	}
+	
+	@Override
+	public void addFile(File fileToBeAdded) throws FileAlreadyExistsException {
+		if(hasFile(fileToBeAdded.getName()))
+			throw new FileAlreadyExistsException(fileToBeAdded.getName());
+		
+		super.addFile(fileToBeAdded);
+	}
+	
+	@Override
+	public void removeFile(File fileToBeRemoved) throws FileDoesNotExistException {
+		if(!hasFile(fileToBeRemoved.getName()))
+			throw new FileDoesNotExistException(fileToBeRemoved.getName());
+		
+		super.removeFile(fileToBeRemoved);
+	}
+
+	public void listFileSet(){
+		
+	}
+	
+	
+	public File getFileByName(String name){
+		for(File file: getFileSet())
+			if(file.getName().equals(name))
+				return file;
+		return null;
+	}
+	
+	public boolean hasFile(String fileName){
+		return getFileByName(fileName) != null;
+	}
+	
 }
