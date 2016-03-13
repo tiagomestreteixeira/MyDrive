583df650c9092fcd211540c2cd16b30f20883043
RenatoL <lourinho.renato@gmail.com>
2016-03-13 18:54:28 +0000

    Added Directory class exceptions

---
+++ src/main/java/pt/tecnico/myDrive/exception/FileAlreadyExistsException.java
@@ -0,0 +1,17 @@
+package pt.tecnico.myDrive.exception;
+
+public class FileAlreadyExistsException extends MyDriveException {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private String conflictingName;
+	
+	public FileAlreadyExistsException(String conflictingName){
+		this.conflictingName = conflictingName;
+	}
+	
+	public String getMessage(){
+		return "The file: '" + this.conflictingName + "' already exists.";
+	}
+	
+}
