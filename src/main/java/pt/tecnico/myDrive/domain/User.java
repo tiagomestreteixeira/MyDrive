595baf2801d3f9ddf772be21a86f887abd193141
Andre Zilhao <aalmsantos@gmail.com>
2016-03-14 18:18:06 +0000

    Refactored Lookup. Renamed Dir creation method to MakeDir().

--- src/main/java/pt/tecnico/myDrive/domain/User.java
+++ src/main/java/pt/tecnico/myDrive/domain/User.java
@@ -106,49 +106,46 @@ public class User extends User_Base {
             md.addUser(this);
     }
 
-    public File lookup(String pathname){
-        String[] params = pathname.split("/");
-        Stack<String> st = new Stack<>();
-        for (int i = params.length-1; i > 0; i--) {
-            log.debug(params[i]);
-            st.push(params[i]);
-			int b = 10;
-        }
-
-        File fd = SuperUser.getInstance().getFileByName("/");
-        while (!st.empty()) {
-            if (fd instanceof Dir)
-                fd = ((Dir) fd).getFileByName(st.pop());
-            //TODO: Check for links.
-            //TODO: Check Permissions.
-        }
-        return fd;
-    }
-
-	public File lookupDir(String pathname){
+	private Stack<String> toStack (String pathname) {
 		String[] params = pathname.split("/");
 		Stack<String> st = new Stack<>();
-		for (int i = params.length-1; i > 0; i--) {
+		for (int i = params.length - 1; i > 0; i--) {
 			log.debug(params[i]);
 			st.push(params[i]);
-			int a = 5;
-
 		}
+		return st;
+	}
 
-		File fd = SuperUser.getInstance().getFileByName("/");
+	public File lookup(String pathname){
+
+		File file = SuperUser.getInstance().getFileByName("/");
+		Stack<String> st = toStack(pathname);
+
 		while (!st.empty()) {
-			if (fd instanceof Dir) {
+			if (file instanceof Dir)
+				file = ((Dir) file).getFileByName(st.pop());
+			//TODO: Check for links.
+			//TODO: Check Permissions.
+		}
+		return file;
+	}
+	public Dir makeDir(String pathname){
+
+		File file = SuperUser.getInstance().getFileByName("/");
+		Stack<String> st = toStack(pathname);
+		while (!st.empty()) {
+			if (file instanceof Dir) {
 				String temp = st.pop();
-				Dir d = (Dir)fd;
-				fd = ((Dir) fd).getFileByName(temp);
-				if (fd == null) {
-					fd = new Dir(temp,this,d,this.getUmask());
+				Dir d = (Dir)file;
+				file = ((Dir) file).getFileByName(temp);
+				if (file == null) {
+					file = new Dir(temp,this,d,this.getUmask());
 				}
 			}
 			//TODO: Check for links.
 			//TODO: Check Permissions.
 		}
-		return fd;
+		return (Dir)file;
 	}
 
     public void xmlImport(String username, Element userElement) throws ImportDocumentException {
