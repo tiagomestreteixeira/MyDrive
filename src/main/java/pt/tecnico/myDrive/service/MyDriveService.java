be6b91b6e3795a6f2a084ff10a424d95a19e89b4
Ivan Santos <vanil90@gmail.com>
2016-04-08 09:31:10 +0100

    Added abstract service.

---
+++ src/main/java/pt/tecnico/myDrive/service/MyDriveService.java
@@ -0,0 +1,33 @@
+package pt.tecnico.myDrive.service;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import pt.ist.fenixframework.Atomic;
+import pt.tecnico.myDrive.domain.MyDrive;
+import pt.tecnico.myDrive.domain.User;
+import pt.tecnico.myDrive.exception.*;
+
+public abstract class MyDriveService {
+	protected static final Logger log = LogManager.getRootLogger();
+
+	@Atomic
+	public final void execute() throws MyDriveException {
+		dispatch();
+	}
+
+	static MyDrive getMyDrive() {
+		return MyDrive.getInstance();
+	}
+
+	static User getUser(String username) throws UserDoesNotExistException {
+		User user = getMyDrive().getUserByUsername(username);
+
+		if (user == null)
+			throw new UserDoesNotExistException(username);
+
+		return user;
+	}
+
+	protected abstract void dispatch() throws MyDriveException;
+}
