630c59f63fb3e1a5f27102bc15030d736eca7879
Ivan Santos <vanil90@gmail.com>
2016-04-11 14:44:42 +0100

    Added basic test class that doen's test anything

---
+++ src/test/java/pt/tecnico/myDrive/service/DeleteFileTest.java
@@ -0,0 +1,17 @@
+package pt.tecnico.myDrive.service;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.*;
+
+public class DeleteFileTest extends AbstractServiceTest {;
+
+	@Override
+	protected void populate() {
+
+	}
+
+	@Test
+	public void success() throws Exception {
+	}
+}
