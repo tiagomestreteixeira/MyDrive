710853f6f5003750e24ede9e1857c156e03fed17
Ivan Santos <vanil90@gmail.com>
2016-04-11 15:15:32 +0100

    Basic DeleteFileService

---
+++ src/main/java/pt/tecnico/myDrive/service/DeleteFileService.java
@@ -0,0 +1,15 @@
+package pt.tecnico.myDrive.service;
+
+import pt.tecnico.myDrive.exception.MyDriveException;
+
+public class DeleteFileService extends MyDriveService {
+
+	public DeleteFileService(long token, String filename) {
+
+	}
+
+	@Override
+	protected void dispatch() throws MyDriveException {
+
+	}
+}
