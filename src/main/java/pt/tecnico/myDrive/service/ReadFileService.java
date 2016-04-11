be58d72dae766f4d84e63051560dbac603a81763
Ivan Santos <vanil90@gmail.com>
2016-04-11 14:00:58 +0100

    Added basic ReadFileService

---
+++ src/main/java/pt/tecnico/myDrive/service/ReadFileService.java
@@ -0,0 +1,18 @@
+package pt.tecnico.myDrive.service;
+
+import pt.tecnico.myDrive.exception.MyDriveException;
+
+public class ReadFileService extends MyDriveService {
+	public ReadFileService(long token, String filename) {
+
+	}
+
+	@Override
+	protected void dispatch() throws MyDriveException {
+
+	}
+
+	public String result() {
+		return null;
+	}
+}
