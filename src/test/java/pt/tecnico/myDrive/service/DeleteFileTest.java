a2cc96d5719c7e864e49286163b980cf1dd4b9a4
Ivan Santos <vanil90@gmail.com>
2016-04-11 18:27:07 +0100

    Tests for DeleteFileService.

--- src/test/java/pt/tecnico/myDrive/service/DeleteFileTest.java
+++ src/test/java/pt/tecnico/myDrive/service/DeleteFileTest.java
@@ -1,13 +1,18 @@
 package pt.tecnico.myDrive.service;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Test;
-import pt.tecnico.myDrive.domain.Dir;
-import pt.tecnico.myDrive.domain.PlainFile;
-import pt.tecnico.myDrive.domain.User;
+import pt.ist.fenixframework.Atomic;
+import pt.tecnico.myDrive.domain.*;
+import pt.tecnico.myDrive.exception.FileDoesNotExistException;
+import pt.tecnico.myDrive.exception.NoPermissionException;
 
+import static junit.framework.TestCase.assertNotNull;
 import static junit.framework.TestCase.assertNull;
 
 public class DeleteFileTest extends AbstractServiceTest {
+	static Logger log = LogManager.getLogger();
 	long login;
 	User u;
 
@@ -15,7 +20,13 @@ public class DeleteFileTest extends AbstractServiceTest {
 	protected void populate() {
 		u = new User(MyDriveService.getMyDrive(), "Ivan");
 		Dir home = u.getHomeDir();
+
 		new PlainFile("test", u, home, u.getUmask());
+		new Link("link", u, home, u.getUmask(), home.getPath() + "test");
+		Dir dir = new Dir("testDir", u, home, u.getUmask());
+		Dir subDir = new Dir("subDir", u, dir, u.getUmask());
+		new PlainFile("test", u, subDir, u.getUmask());
+
 		LoginUserService service = new LoginUserService("Ivan", "Ivan");
 		service.execute();
 		login = service.result();
@@ -26,5 +37,50 @@ public class DeleteFileTest extends AbstractServiceTest {
 		DeleteFileService service = new DeleteFileService(login, "test");
 		service.execute();
 		assertNull("File should not exist", u.lookup("/home/Ivan/test"));
+	}
+
+	@Test
+	public void deleteLinkFile() throws Exception {
+		DeleteFileService service = new DeleteFileService(login, "link");
+		service.execute();
+		assertNull("File 'link' should not exist", u.lookup("/home/Ivan/link"));
+		assertNotNull("File 'test' should exist", u.lookup("/home/Ivan/test"));
+	}
+
+	@Test
+	public void deleteDirAndSubDirs() throws Exception {
+		DeleteFileService service = new DeleteFileService(login, "testDir");
+		service.execute();
+		assertNull("testDir should not exist", u.lookup("/home/Ivan/testDir"));
+	}
+
+	@Test(expected = NoPermissionException.class)
+	public void deleteFileNoOwnerNoPermission() throws Exception {
+		SuperUser root = MyDriveService.getMyDrive().getSuperUser();
+		new PlainFile("test1", root, u.getHomeDir(), "rwxd----");
+		DeleteFileService service = new DeleteFileService(login, "test1");
+		service.execute();
+	}
+
+	@Test(expected = NoPermissionException.class)
+	public void deleteFileNoPermission() throws Exception {
+		new PlainFile("test1", u, u.getHomeDir(), "----rwxd");
+		DeleteFileService service = new DeleteFileService(login, "test1");
+		service.execute();
+	}
+
+	@Test
+	public void deleteFileNoOwnerWithPermission() throws Exception {
+		SuperUser root = MyDriveService.getMyDrive().getSuperUser();
+		new PlainFile("test1", root, u.getHomeDir(), "rwxdrwxd");
+		DeleteFileService service = new DeleteFileService(login, "test1");
+		service.execute();
+		assertNull("File 'test1' should not exist", root.lookup("/home/Ivan/test1"));
+	}
+
+	@Test(expected = FileDoesNotExistException.class)
+	public void deleteNonExistingFile() throws Exception {
+		DeleteFileService service = new DeleteFileService(login, "test1");
+		service.execute();
 	}
 }
