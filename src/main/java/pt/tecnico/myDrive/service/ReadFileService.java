abb7a4db6158d0a41123a3a445ba999271137256
Ivan Santos <vanil90@gmail.com>
2016-04-12 21:48:58 +0100

    Implemented ReadFileService
    closes #54

--- src/main/java/pt/tecnico/myDrive/service/ReadFileService.java
+++ src/main/java/pt/tecnico/myDrive/service/ReadFileService.java
@@ -1,18 +1,30 @@
 package pt.tecnico.myDrive.service;
 
+import pt.tecnico.myDrive.domain.Dir;
+import pt.tecnico.myDrive.domain.Login;
+import pt.tecnico.myDrive.domain.User;
 import pt.tecnico.myDrive.exception.MyDriveException;
 
 public class ReadFileService extends MyDriveService {
-	public ReadFileService(long token, String filename) {
+	private User user;
+	private Dir currentDir;
+	private String filename;
 
+	private String result;
+
+	public ReadFileService(long token, String filename) {
+		Login login = getMyDrive().getLoginFromId(token);
+		user = login.getUser();
+		currentDir = login.getCurrentDir();
+		this.filename = filename;
 	}
 
 	@Override
 	protected void dispatch() throws MyDriveException {
-
+		result = currentDir.getFileByName(user, filename).read(user);
 	}
 
 	public String result() {
-		return null;
+		return result;
 	}
 }
