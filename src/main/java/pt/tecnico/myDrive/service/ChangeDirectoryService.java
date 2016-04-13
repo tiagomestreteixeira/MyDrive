3fa30d4a6c15224886fd586a76422a90f63cbc3c
Ivan Santos <vanil90@gmail.com>
2016-04-13 13:27:18 +0100

    Implemented ChangeDirectoryService
    closes #43

--- src/main/java/pt/tecnico/myDrive/service/ChangeDirectoryService.java
+++ src/main/java/pt/tecnico/myDrive/service/ChangeDirectoryService.java
@@ -1,18 +1,36 @@
 package pt.tecnico.myDrive.service;
 
+import pt.tecnico.myDrive.domain.Dir;
+import pt.tecnico.myDrive.domain.Login;
+import pt.tecnico.myDrive.domain.User;
 import pt.tecnico.myDrive.exception.MyDriveException;
 
 public class ChangeDirectoryService extends MyDriveService {
-	public ChangeDirectoryService(long token, String pathname) {
+	private Login login;
+	private User user;
+	private Dir currentDir;
+	private String pathname;
 
+	private String result;
+
+	public ChangeDirectoryService(long token, String pathname) {
+		login = getMyDrive().getLoginFromId(token);
+		user = login.getUser();
+		currentDir = login.getCurrentDir();
+		if (pathname.startsWith("/"))
+			this.pathname = pathname;
+		else
+			this.pathname = currentDir.getPath() + "/" + pathname;
 	}
 
 	@Override
 	protected void dispatch() throws MyDriveException {
-
+		Dir d = (Dir) user.lookup(pathname);
+		login.setCurrentDir(d);
+		result = d.getPath();
 	}
 
 	public String result() {
-		return null;
+		return result;
 	}
 }
