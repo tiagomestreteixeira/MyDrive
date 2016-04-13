cc6cad527fa7c5c1984d832a0156af0fbea8c4ad
André Filipe Salgado de Ascenção <andrefsascensao@gmail.com>
2016-04-13 19:10:10 +0100

    Login User Service

--- src/main/java/pt/tecnico/myDrive/service/LoginUserService.java
+++ src/main/java/pt/tecnico/myDrive/service/LoginUserService.java
@@ -2,19 +2,49 @@ package pt.tecnico.myDrive.service;
 
 
 import pt.tecnico.myDrive.exception.MyDriveException;
+import pt.tecnico.myDrive.domain.*;
+
+
 
 public class LoginUserService extends MyDriveService {
+	 private String username;
+	 private String password;
+	 private MyDrive md;
+	 private long token;
+	
 	public LoginUserService(String username, String Password) {
-
-
+	    this.username=username;
+	    this.password=password;
+	    this.md=MyDrive.getInstance();
 	}
+	    @Override
+		protected void dispatch() throws MyDriveException {
+	    
+	    md.getUserByUsername(username);
+	    User user = md.getUserByUsername(username);
+	    
+	    String pass= user.getPassword();
+	    
+	    if(pass == null || !(pass.equals(this.password))){
+	      	user.setPassword(password);
+	    }
+	    token = md.createLogin(this.username, this.password);
+			
+		}
+	
+	  
+		protected void validateAndAuthorize(String token) {
+			return;
+		}
+	  
+	
+		protected void checkAccess() {
+			return;
+		}
 
-	@Override
-	protected void dispatch() throws MyDriveException {
-
-	}
 
 	public long result() {
 		return 0;
 	}
 }
+
