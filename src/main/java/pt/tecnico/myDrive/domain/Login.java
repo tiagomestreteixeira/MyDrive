ddaee6bd456e2f67fac7e1c31ffdefd21115cbcb
AndreZilhao <aalmsantos@gmail.com>
2016-04-13 16:27:58 +0100

    Fixed Refresh Token to simply change the date of the token.

--- src/main/java/pt/tecnico/myDrive/domain/Login.java
+++ src/main/java/pt/tecnico/myDrive/domain/Login.java
@@ -12,25 +12,25 @@ import java.util.Random;
 import pt.tecnico.myDrive.domain.User;
 
 public class Login extends Login_Base {
-    
+
 	static final Logger log = LogManager.getRootLogger();
-	
+
     public Login(User user) {
         super();
-        
+
         Long tokenId = new BigInteger(64, new Random()).longValue();
-        
+
         this.setUser(user);
         this.setLoginDate(new DateTime());
      	this.setIdentifier(tokenId);
      	this.setCurrentDir(user.getHomeDir());
     }
-    
+
     public Login(User user, long oldToken) {
         super();
-        
+
         Long tokenId = new BigInteger(64, new Random()).longValue();
-        
+
         this.setUser(user);
         this.setLoginDate(new DateTime());
         if (user.getUsername().equals("root")){
@@ -46,19 +46,11 @@ public class Login extends Login_Base {
         	}
         }
      }
-    
-    public void refreshToken(){
-    		Long tokenId = new BigInteger(64, new Random()).longValue();
-    		
-	    	if (this.getIdentifier() == tokenId){
-	    		this.refreshToken();
-	    	}
-	    	else{
-	    		this.setIdentifier(tokenId);
-	        	this.setLoginDate(new DateTime());
-	    	}
-    	}	
-    
+
+	public void refreshToken() {
+		this.setLoginDate(new DateTime());
+	}
+
     public boolean isDateValid(DateTime data){
     	double hours = (data.getMillis() - this.getLoginDate().getMillis()) /1000/60/60;
 		if(hours>2){
@@ -66,16 +58,16 @@ public class Login extends Login_Base {
 		}
 		return true;
     }
-    
+
     public void delete(){
     	this.setUser(null);
     	this.setCurrentDir(null);
-    	
+
     	for(EnvVariables env : super.getEnvVarSet()){
 	    	super.removeEnvVar(env);
 	    	//env.delete();
 	    }
-    	
+
     	deleteDomainObject();
     }
 }
