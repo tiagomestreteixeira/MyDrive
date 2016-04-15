ba6640a75a28d06dd7cdd6734655aae26f6201fa
andrefsascensao <andrefsascensao@gmail.com>
2016-04-15 14:47:42 +0100

    Changed ListDirectoryTest. Removed pathname from ListDirectoryService

--- src/test/java/pt/tecnico/myDrive/service/ListDirectoryTest.java
+++ src/test/java/pt/tecnico/myDrive/service/ListDirectoryTest.java
@@ -14,60 +14,76 @@ import pt.tecnico.myDrive.exception.*;
 public class ListDirectoryTest extends AbstractServiceTest{
 
 	static Logger log = LogManager.getLogger();
-	private long login;
+	private long loginId;
 	private String user;
 	private User userObject;
-	private SuperUser root;
 	private MyDrive md;
+	private Login login;
 	private String testFile = "testPlainFile";
 	private String testString;
-	private ArrayList<String> fileList = new ArrayList<String>();
+	private ArrayList<String> testFileList = new ArrayList<String>();
 	private static final String USER_DEFAULT_PERMISSIONS = "rwxd----";
 
-	
+
 	@Override
 	protected void populate() {
 		user = "Andre";
 		md = MyDriveService.getMyDrive();
 		userObject = new User(md, user);
-		login = md.createLogin(user,user);
-		root = MyDriveService.getMyDrive().getSuperUser();
+		loginId = md.createLogin(user,user);
+		Login login = MyDriveService.getMyDrive().getLoginFromId(loginId);
+		
 		Dir dir = new Dir("teste", userObject, userObject.getHomeDir(), userObject.getUmask());
 		PlainFile test = new PlainFile(testFile, userObject, userObject.getHomeDir(), USER_DEFAULT_PERMISSIONS, "contentOf:\n\nPlainFile");
-		
+
 		testString = "Dir " + test.getPermissions() + " " + test.getFileOwner() + " " + test.getId() + " " + test.getName();
 	}
 
 	@Test
 	public void success(){
-		final String pathname = "/home/Andre/";
-		fileList.add(testString);
-		ListDirectoryService service = new ListDirectoryService(login, pathname);
+		testFileList.add(testString);
+		ListDirectoryService service = new ListDirectoryService(loginId);
 		service.execute();
 		ArrayList<String> result = service.result();
-		assertEquals("Directory listing is not the same", result, fileList);
+		assertEquals("Directory listing is not the same", result, testFileList);
 	}
-	
+
 	@Test(expected=DirectoryHasNoFilesException.class)
 	public void listEmptyDirectory(){
 		final String pathname = "/home/Andre/Empty";
 		new Dir("Empty", userObject, userObject.getHomeDir(),USER_DEFAULT_PERMISSIONS );
-		ListDirectoryService service = new ListDirectoryService(login, pathname);
+		changeDir(loginId, pathname);
+		ListDirectoryService service = new ListDirectoryService(loginId);
 		service.execute();		
 	}
-	
+
 	@Test(expected=NoPermissionException.class)
 	public void noPermissionDirectoryList(){
 		final String pathname = "/home/Andre/NoPerm";
 		new Dir("NoPerm", userObject, userObject.getHomeDir(), "--------");
-		ListDirectoryService service = new ListDirectoryService(login, pathname);
+		changeDir(loginId, pathname);
+		ListDirectoryService service = new ListDirectoryService(loginId);
 		service.execute();
 	}
-	
+
 	@Test(expected=FileDoesNotExistException.class)
 	public void NonExistantDirectoryList(){
 		final String pathname = "/home/Andre/NA";
-		ListDirectoryService service = new ListDirectoryService(login, pathname);
+		changeDir(loginId, pathname);
+		ListDirectoryService service = new ListDirectoryService(loginId);
 		service.execute();
+	}
+
+	public void changeDir(long loginId, String pathname){
+		
+		login.refreshToken();
+		User user = login.getUser();
+		Dir currentDir = login.getCurrentDir();
+
+		if (!pathname.startsWith("/"))
+			pathname = currentDir.getPath() + "/" + pathname;
+
+		Dir d = (Dir) user.lookup(pathname);
+		login.setCurrentDir(d);
 	}
 }
