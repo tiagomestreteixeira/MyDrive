b72727cb03eaaac9fd73d9efe045f6d13210bcf1
andrefsascensao <andrefsascensao@gmail.com>
2016-04-15 01:43:23 +0100

    Added necessary test variables.

--- src/test/java/pt/tecnico/myDrive/service/ListDirectoryTest.java
+++ src/test/java/pt/tecnico/myDrive/service/ListDirectoryTest.java
@@ -1,5 +1,7 @@
 package pt.tecnico.myDrive.service;
 
+import static org.junit.Assert.*;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.joda.time.DateTime;
@@ -15,6 +17,11 @@ public class ListDirectoryTest extends AbstractServiceTest{
 	private User userObject;
 	private SuperUser root;
 	private MyDrive md;
+	private String testFile = "testPlainFile";
+	private String testString;
+			
+	private static final String USER_DEFAULT_PERMISSIONS = "rwxd----";
+
 	
 	@Override
 	protected void populate() {
@@ -24,7 +31,11 @@ public class ListDirectoryTest extends AbstractServiceTest{
 		login = md.createLogin(user,user);
 		root = MyDriveService.getMyDrive().getSuperUser();
 		Dir dir = new Dir("teste", userObject, userObject.getHomeDir(), userObject.getUmask());
+		PlainFile test = new PlainFile(testFile, userObject, userObject.getHomeDir(), USER_DEFAULT_PERMISSIONS, "contentOf:\n\nPlainFile");
 		
+		testString = "Dir " + test.getPermissions() + " " + test.getFileOwner() + " " + test.getId() + " " + test.getName();
 	}
 
+
+	
 }
