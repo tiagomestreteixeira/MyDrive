80e4ded50a64969456c32aa0b2c3ed0814037b79
RenatoL <lourinho.renato@gmail.com>
2016-04-15 15:39:07 +0100

    Changed ListDirectoryService to use and return a FileDto list.

--- src/main/java/pt/tecnico/myDrive/service/ListDirectoryService.java
+++ src/main/java/pt/tecnico/myDrive/service/ListDirectoryService.java
@@ -1,40 +1,56 @@
 package pt.tecnico.myDrive.service;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import pt.tecnico.myDrive.domain.App;
 import pt.tecnico.myDrive.domain.Dir;
+import pt.tecnico.myDrive.domain.File;
+import pt.tecnico.myDrive.domain.Link;
 import pt.tecnico.myDrive.domain.Login;
+import pt.tecnico.myDrive.domain.PlainFile;
 import pt.tecnico.myDrive.exception.MyDriveException;
+import pt.tecnico.myDrive.service.dto.FileDto;
 
 public class ListDirectoryService extends MyDriveService {
 
 	private long loginId;
-	private ArrayList<String> fileList;
-	private ArrayList<String> result;
+	private List<FileDto> fileList;
 	private Dir currentDir;
 
 	public ListDirectoryService(long token) {
 		this.loginId = token;
-		fileList = new ArrayList<String>();
+
 	}
 
 	@Override
 	protected void dispatch() throws MyDriveException {
+		fileList = new ArrayList<FileDto>();
+
 		Login login = getMyDrive().getLoginFromId(loginId);
 
 		login.refreshToken();
 		currentDir = login.getCurrentDir();
 
-		fileList = currentDir.listFileSet();
+		for(File f : currentDir.getFileSet()){
 
-		for(String file : fileList){
-			System.out.println(file);
+			if(f instanceof Dir){
+				fileList.add((new FileDto(f.getId(), f.getName(), f.getLastModification(), f.getPermissions(), "Dir")));
+			}
+			if(f instanceof PlainFile){
+				fileList.add((new FileDto(f.getId(), f.getName(), f.getLastModification(), f.getPermissions(), "PlainFile", ((PlainFile) f).getContent())));
+			}
+			if(f instanceof Link){
+				fileList.add((new FileDto(f.getId(), f.getName(), f.getLastModification(), f.getPermissions(), "Link", ((Link) f).getContent())));
+			}
+			if(f instanceof App){
+				fileList.add((new FileDto(f.getId(), f.getName(), f.getLastModification(), f.getPermissions(), "App", ((App) f).getContent())));
+			}
 		}
 
-		result = fileList;
 	}
 
-	public ArrayList<String> result() {
-		return result;
+	public List<FileDto> result() {
+		return fileList;
 	}
 }
