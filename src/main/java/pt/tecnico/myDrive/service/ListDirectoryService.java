2a849e65abe40a7e6d2c330d535ecf943fe02909
RenatoL <lourinho.renato@gmail.com>
2016-04-15 14:14:29 +0100

    Removed login token validity check from ListDirectoryService and DeleteFileService - merge issue after Login refactor.

--- src/main/java/pt/tecnico/myDrive/service/ListDirectoryService.java
+++ src/main/java/pt/tecnico/myDrive/service/ListDirectoryService.java
@@ -7,13 +7,13 @@ import pt.tecnico.myDrive.domain.Login;
 import pt.tecnico.myDrive.exception.MyDriveException;
 
 public class ListDirectoryService extends MyDriveService {
-	
+
 	private long loginId;
 	private String pathname;
 	private ArrayList<String> fileList;
 	private ArrayList<String> result;
 	private Dir currentDir;
-	
+
 	public ListDirectoryService(long token, String pathname) {
 		this.loginId = token;
 		this.pathname = pathname;
@@ -23,18 +23,16 @@ public class ListDirectoryService extends MyDriveService {
 	@Override
 	protected void dispatch() throws MyDriveException {
 		Login login = getMyDrive().getLoginFromId(loginId);
-		if (getMyDrive().isTokenValid(loginId)) {
-			login.refreshToken();
-			currentDir = login.getCurrentDir();
-		}
-		
-		
+
+		login.refreshToken();
+		currentDir = login.getCurrentDir();
+
 		fileList = currentDir.listFileSet();
-		
+
 		for(String file : fileList){
 			System.out.println(file);
 		}
-		
+
 		result = fileList;
 	}
 
