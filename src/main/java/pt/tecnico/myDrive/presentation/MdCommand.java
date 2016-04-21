94b151d778aff0a5ca80d392f7e0fe2f35c87750
tiagomestreteixeira <tam.teixeira@gmail.com>
2016-04-21 21:58:32 +0100

    Added the abstract MdCommand - Extending the Command Class

---
+++ src/main/java/pt/tecnico/myDrive/presentation/MdCommand.java
@@ -0,0 +1,9 @@
+package pt.tecnico.phonebook.presentation;
+
+import pt.tecnico.myDrive.presentation.Command;
+import pt.tecnico.myDrive.presentation.Shell;
+
+public abstract class MdCommand extends Command {
+    public MdCommand(Shell sh, String n) { super(sh, n); }
+    public MdCommand(Shell sh, String n, String h) { super(sh, n, h); }
+}
