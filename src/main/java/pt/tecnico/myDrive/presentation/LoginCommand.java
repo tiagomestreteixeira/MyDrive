3acfb6da76f3c4d7cac496b00e16e646dea37c9b
tiagomestreteixeira <tam.teixeira@gmail.com>
2016-04-22 01:44:13 +0100

    Login Command : Only allow exactly 2 arguments to the command and usage message Updated

--- src/main/java/pt/tecnico/myDrive/presentation/LoginCommand.java
+++ src/main/java/pt/tecnico/myDrive/presentation/LoginCommand.java
@@ -3,10 +3,15 @@ import pt.tecnico.myDrive.service.LoginUserService;
 
 public class LoginCommand extends MdCommand{
 
-    public LoginCommand(Shell sh) { super(sh, "Login", "Log into the system providing the username and login token "); }
+    private static final String USAGE_MSG = "USAGE: login username [password]\n";
+
+    public LoginCommand(Shell sh) {
+        super(sh, "login", "Log into the system providing the username and login token.\n" + USAGE_MSG);
+    }
+
     public void execute(String[] args) {
         if (args.length != 2)
-            throw new RuntimeException("USAGE: "+name()+" username");
+            throw new RuntimeException(USAGE_MSG);
         new LoginUserService(args[0],args[1]).execute();
     }
 }
