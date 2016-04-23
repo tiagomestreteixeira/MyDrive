76499a6043a16ba1359d8a32041415ab63b4143c
tiagomestreteixeira <tam.teixeira@gmail.com>
2016-04-23 15:48:54 +0100

    Refactored Login

--- src/main/java/pt/tecnico/myDrive/presentation/LoginCommand.java
+++ src/main/java/pt/tecnico/myDrive/presentation/LoginCommand.java
@@ -10,24 +10,21 @@ public class LoginCommand extends MdCommand{
         super(sh, "login", "Log into the system providing the username and login token.\n" + USAGE_MSG);
     }
 
-    public void execute(String[] args) throws Exception {
-        LoginUserService loginUserService;
-        if (args.length != 2){
-            if(args.length == 1 && args[0].equals("Guest")) {
-                loginUserService = new LoginUserService(args[0], null);
-                loginUserService.execute();
-                token = loginUserService.result();
-                return;
-            }
-            throw new RuntimeException(USAGE_MSG);
-        }
 
-        if(args.length > 2)
-            throw new RuntimeException(USAGE_MSG);
-
-        loginUserService = new LoginUserService(args[0],args[1]);
+    private void executeLogin(String user, String pass){
+        LoginUserService loginUserService = new LoginUserService(user, pass);
         loginUserService.execute();
         token = loginUserService.result();
-        this.println("Token generated: " + token.toString() + "\n");
+        log.info("Token generated: ", token.toString());
+    }
+
+    public void execute(String[] args) throws Exception {
+        if (args.length != 2){
+            if(args.length == 1 && args[0].equals("Guest"))
+                executeLogin("nobody",null);
+            else
+                throw new RuntimeException(USAGE_MSG);
+        }
+        executeLogin(args[0],args[1]);
     }
 }
