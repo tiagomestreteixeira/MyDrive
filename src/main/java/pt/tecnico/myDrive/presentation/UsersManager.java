d541a51e69aa7ee4a973b8a7d330653511dce4b0
tiagomestreteixeira <tam.teixeira@gmail.com>
2016-04-26 01:57:41 +0100

    Added UserManagerPresentationException and corrected error messages

--- src/main/java/pt/tecnico/myDrive/presentation/UsersManager.java
+++ src/main/java/pt/tecnico/myDrive/presentation/UsersManager.java
@@ -1,5 +1,7 @@
 package pt.tecnico.myDrive.presentation;
 
+import pt.tecnico.myDrive.exception.UserManagerPresentationException;
+
 import java.util.HashMap;
 
 public class UsersManager {
@@ -8,12 +10,12 @@ public class UsersManager {
     private String currentUsername;
     private Long currentToken;
 
-    private static final String GET_TOKEN_MSG = "Presentation : addUser - UserManager ";
-    private static final String UPDATE_TOKEN_MSG = "Presentation : updateToken - UserManager ";
-    private static final String SET_CURRENT_USERNAME_MSG  = "Presentation : setCurrentUsername - User ";
-    private static final String SET_CURRENT_TOKEN_MSG  = "Presentation : setCurrentToken - User \"Illegal usage. Use updateToken(username,token) instead.\\n\"";
+    private static final String GET_TOKEN_MSG = "Presentation : getTokenByUsername - ";
+    private static final String UPDATE_TOKEN_MSG = "Presentation : updateToken - ";
+    private static final String SET_CURRENT_USERNAME_MSG  = "Presentation : setCurrentUsername - ";
+    private static final String SET_CURRENT_TOKEN_MSG  = "Presentation : setCurrentToken - \"Illegal usage. Use updateToken(username,token) instead.\n";
 
-    private static final String NOT_LOGGED_SYSTEM_MSG = " has not been logged into the system.\n\n";
+    private static final String NOT_LOGGED_SYSTEM_MSG = " - has not been previously logged into the system.\n\n";
 
     private static final String GET_TOKEN_HELP_MSG = "\t\t- First add username and its token : addUser(";
     private static final String UPDATE_TOKEN_HELP_MSG = GET_TOKEN_HELP_MSG;
@@ -22,10 +24,9 @@ public class UsersManager {
         users = new HashMap<String, Long>();
     }
 
-    private void checkExistence(String username, String exMsg) throws Exception
-    {
+    private void checkUserExistence(String username, String exMsg) throws UserManagerPresentationException {
         if (!users.containsKey(username))
-            throw new Exception(exMsg);
+            throw new UserManagerPresentationException(exMsg);
     }
 
 
@@ -40,36 +41,36 @@ public class UsersManager {
     //    users.put(username,token);
     //}
     // Updates the token Implementation:
-    public void addUser(String username,Long token) throws Exception{
+    public void addUser(String username,Long token) {
         users.put(username,token);
         currentUsername = username;
         currentToken = token;
     }
 
 
-    public Long getToken(String username) throws Exception{
-        checkExistence(username,GET_TOKEN_MSG + username + NOT_LOGGED_SYSTEM_MSG + GET_TOKEN_HELP_MSG
+    public Long getTokenByUsername(String username) throws UserManagerPresentationException {
+        checkUserExistence(username,GET_TOKEN_MSG + username + NOT_LOGGED_SYSTEM_MSG + GET_TOKEN_HELP_MSG
                 + username + ",token[Long])\n");
 
         return users.get(username);
     }
 
-    public void updateToken(String username,Long token) throws Exception{
-        checkExistence(username,UPDATE_TOKEN_MSG + username
-                + NOT_LOGGED_SYSTEM_MSG + UPDATE_TOKEN_HELP_MSG + username + ",token)\n");
+    public void updateTokenByUsername(String username,Long token) throws UserManagerPresentationException{
+        checkUserExistence(username,UPDATE_TOKEN_MSG + username
+                + NOT_LOGGED_SYSTEM_MSG + UPDATE_TOKEN_HELP_MSG + username + ",token[Long])\n");
 
         users.put(username,token);
     }
 
-    public void setCurrentUsername(String username) throws Exception{
-        if(!users.containsKey(username))
-            throw new Exception(SET_CURRENT_USERNAME_MSG + username + NOT_LOGGED_SYSTEM_MSG + "\n");
+    public void setCurrentUsername(String username) throws UserManagerPresentationException{
+        checkUserExistence(username,SET_CURRENT_USERNAME_MSG + username + NOT_LOGGED_SYSTEM_MSG + "\n");
+
         currentUsername = username;
         currentToken = users.get(username);
     }
 
-    public void setCurrentToken(Long token) throws Exception {
-        throw new Exception(SET_CURRENT_TOKEN_MSG);
+    public void setCurrentToken(Long token) throws UserManagerPresentationException {
+        throw new UserManagerPresentationException(SET_CURRENT_TOKEN_MSG);
     }
 
     public String getCurrentUsername(){
