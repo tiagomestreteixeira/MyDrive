66cdacfaed8c3cf72190abd75dc885cbb20cda20
tiagomestreteixeira <tam.teixeira@gmail.com>
2016-05-04 22:07:31 +0100

    System-Integration-Test : Logins and ListDirectory Service sequence Tested. Additional helper methods/fields added

--- src/test/java/pt/tecnico/myDrive/integration/IntegrationTest.java
+++ src/test/java/pt/tecnico/myDrive/integration/IntegrationTest.java
@@ -7,27 +7,22 @@ import org.jdom2.Element;
 import org.jdom2.JDOMException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import mockit.Mock;
-import mockit.MockUp;
 import mockit.integration.junit4.JMockit;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
 
 import org.jdom2.Document;
 import org.jdom2.input.SAXBuilder;
-import org.jdom2.output.XMLOutputter;
-import org.jdom2.output.Format;
 
 
 import pt.tecnico.myDrive.Main;
 import pt.tecnico.myDrive.domain.MyDrive;
 import pt.tecnico.myDrive.domain.SuperUser;
-import pt.tecnico.myDrive.domain.User;
 import pt.tecnico.myDrive.exception.ImportDocumentException;
 import pt.tecnico.myDrive.service.*;
+import pt.tecnico.myDrive.service.dto.FileDto;
 
 @RunWith(JMockit.class)
 public class IntegrationTest extends AbstractServiceTest {
@@ -35,68 +30,93 @@ public class IntegrationTest extends AbstractServiceTest {
     private MyDrive md;
     private SuperUser su;
 
-    private User user_tiago;
-    private User user_andre;
+    private int jtb_index;
+    private int mja_index;
+    private int blm_index;
+    private int sbp_index;
+    private int jcc_index;
+    private int aja_index;
+    private int mtg_index;
 
-    private static final String username_jtb = "jtb", username_mja = "mja";
-    private static final String password_jtb = "Fernandes", password_mja = "Peyrelongue";
-    private Long token_jtb, token_mja;
-
-    private static final String importXMLFilename = "users.xml";
+    private static final String IMPORT_XML_FILENAME = "users.xml";
 
     private class UserInfo{
         public String username, password;
         public Long token;
+        public int numberFilesHomeDir;
         UserInfo(){};
+    }
+
+    private int indexOfByUsername(String username) {
+        int i = 0;
+        for (UserInfo ui : users){
+            if (ui.username.equals(username))
+                return i;
+            i++;
+        }
+        return -1;
     }
 
     private static final List<UserInfo> users = new ArrayList<UserInfo>();
 
-    private void UsersXMLtoList() {
+    private void usersXMLtoList() {
         SAXBuilder builder = new SAXBuilder();
         try {
-            Document document = (Document) builder.build(Main.resourceFile(importXMLFilename));
+            Document document = (Document) builder.build(Main.resourceFile(IMPORT_XML_FILENAME));
             for (Element node : document.getRootElement().getChildren("user")) {
                 UserInfo ui = new UserInfo();
                 ui.username = node.getAttribute("username").getValue();;
                 ui.password = node.getChild("password").getValue();
                 ui.token = null;
+                ui.numberFilesHomeDir = 0;
                 users.add(ui);
             }
 
         }catch(ImportDocumentException | JDOMException | IOException e){
             e.printStackTrace();
         }
+        //log.debug("INDEX:" + indexOfByUsername("jtb"));
+        //log.info("TJB" + users.get(indexOfByUsername("jtb")).numberFilesHomeDir);
+        users.get(indexOfByUsername("jtb")).numberFilesHomeDir = 4;
     }
-
-
 
     protected void populate() {
 
         md = MyDrive.getInstance();
         su = md.getSuperUser();
-        UsersXMLtoList();
+        usersXMLtoList();
 
-
-
-        //user_tiago = new User(md, username_tiago);
-        //user_andre = new User(md, username_andre);
-        //users.add(user_tiago);
-        //users.add(user_andre);
-
-        Main.importXML(Main.resourceFile(importXMLFilename));
+        Main.importXML(Main.resourceFile(IMPORT_XML_FILENAME));
     }
 
     @Test
     public void success() throws Exception {
-       //token_jtb = new LoginUserService();
+
+        log.debug("USERS:");
         for(UserInfo ui : users){
-            log.info("User:");
-            log.info("username: " + ui.username);
-            log.info("password: " + ui.password);
-            log.info("token: " + ui.token);
+            LoginUserService us = new LoginUserService(ui.username,ui.password);
+            us.execute();
+            assertNotNull(us.result());
+            ui.token = us.result();
+            log.debug("username: " + ui.username);
+            log.debug("password: " + ui.password);
+            log.debug("token: " + ui.token);
+            log.debug("Number of Files in Home dir: " + ui.numberFilesHomeDir);
+
         }
+
+        UserInfo infoJtb = users.get(indexOfByUsername("jtb"));
+        System.out.println("List current non-empty HomeDir Files of : User " + infoJtb.username);
+
+        ListDirectoryService lds = new ListDirectoryService(infoJtb.token);
+        lds.execute();
+
+        for (FileDto dto : lds.result())
+            System.out.println("\t" + dto.getType() + " -> " + dto.getFilename());
+        assertEquals(lds.result().size(), infoJtb.numberFilesHomeDir);
+
 
     }
 
 }
+
