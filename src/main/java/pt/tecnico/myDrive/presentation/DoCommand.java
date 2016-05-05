33e26c37532454ac82ebe8c894eed0607fca153c
Ivan Santos <vanil90@gmail.com>
2016-05-05 13:11:23 +0100

    Added basic structure for the remaining presentation commands.

---
+++ src/main/java/pt/tecnico/myDrive/presentation/DoCommand.java
@@ -0,0 +1,13 @@
+package pt.tecnico.myDrive.presentation;
+
+public class DoCommand extends MdCommand {
+
+	public DoCommand(Shell sh) {
+		super(sh, "do");
+	}
+
+	@Override
+	void execute(String[] args) throws Exception {
+
+	}
+}
---
+++ src/main/java/pt/tecnico/myDrive/presentation/UpdateCommand.java
@@ -0,0 +1,13 @@
+package pt.tecnico.myDrive.presentation;
+
+public class UpdateCommand extends MdCommand {
+
+	public UpdateCommand(Shell sh) {
+		super(sh, "cwd");
+	}
+
+	@Override
+	void execute(String[] args) throws Exception {
+
+	}
+}
