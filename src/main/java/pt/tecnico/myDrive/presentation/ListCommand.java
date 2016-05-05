5cd885845eea274222ebbac4b7adb493383a825c
RenatoL <lourinho.renato@gmail.com>
2016-05-05 22:26:49 +0100

    Added ListCommand to MdShell. Added ListDirectoryService call to ListCommand.

--- src/main/java/pt/tecnico/myDrive/presentation/ListCommand.java
+++ src/main/java/pt/tecnico/myDrive/presentation/ListCommand.java
@@ -1,16 +1,29 @@
 package pt.tecnico.myDrive.presentation;
 
+import pt.tecnico.myDrive.service.ListDirectoryService;
+
 public class ListCommand extends MdCommand {
 
 	private static final String USAGE_MSG = "USAGE: ls [path]\n";
 
     public ListCommand(Shell sh) {
-        super(sh, "list", "List current directory if no argument is given or list given path directory." + USAGE_MSG);
+        super(sh, "list", "List given path directory contents or current directory if no argument is given. " + USAGE_MSG);
     }
 
+    private void executeList(Long token){
+    	ListDirectoryService listDirectoryService = new ListDirectoryService(token);
+    	listDirectoryService.execute();
+    	shell().println("HI");
+    }
+    
 	@Override
 	void execute(String[] args) throws Exception {
-		// TODO Auto-generated method stub
+		if(args.length>1)
+            throw new RuntimeException(USAGE_MSG);
 		
+        
+       
+		Long token = shell().getCurrentToken();
+        executeList(token);
 	}
 }
