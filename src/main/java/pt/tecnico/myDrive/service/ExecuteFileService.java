af3b5b2d8f06a841faed116f88baa73554f7b9d7
tiagomestreteixeira <tam.teixeira@gmail.com>
2016-05-09 12:03:56 +0100

    ExecuteFileService reAdded to Master

---
+++ src/main/java/pt/tecnico/myDrive/service/ExecuteFileService.java
@@ -0,0 +1,31 @@
+package pt.tecnico.myDrive.service;
+
+import pt.tecnico.myDrive.domain.*;
+import pt.tecnico.myDrive.exception.DirCanNotHaveContentException;
+import pt.tecnico.myDrive.exception.InvalidFileTypeException;
+import pt.tecnico.myDrive.exception.MyDriveException;
+
+public class ExecuteFileService extends MyDriveService {
+
+    private long token;
+    private String filename;
+    private String fileType;
+    private String content;
+
+    public ExecuteFileService(long token, String filename, String fileType, String content) {
+        this.token = token;
+        this.filename = filename;
+        this.fileType = fileType;
+        this.content = content;
+    }
+
+
+    @Override
+    protected void dispatch() throws MyDriveException {
+        MyDrive md = getMyDrive();
+        Login login = md.getLoginFromId(token);
+        login.refreshToken();
+        User user = login.getUser();
+        Dir currentDir = login.getCurrentDir();
+    }
+}
