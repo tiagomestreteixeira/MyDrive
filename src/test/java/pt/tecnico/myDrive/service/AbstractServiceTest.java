1e715224b130735da4614476db213b9ab8e851ae
AndreZilhao <aalmsantos@gmail.com>
2016-05-13 15:23:14 +0100

    Changed Cleanup call in abstract service to now use the new cleanup service. closes #171 closes #170

--- src/test/java/pt/tecnico/myDrive/service/AbstractServiceTest.java
+++ src/test/java/pt/tecnico/myDrive/service/AbstractServiceTest.java
@@ -1,47 +1,44 @@
 package pt.tecnico.myDrive.service;
 
-import javax.transaction.NotSupportedException;
-import javax.transaction.SystemException;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
-
 import pt.ist.fenixframework.FenixFramework;
 import pt.ist.fenixframework.core.WriteOnReadError;
-import pt.tecnico.myDrive.Main;
-import pt.tecnico.myDrive.domain.MyDrive;
+
+import javax.transaction.NotSupportedException;
+import javax.transaction.SystemException;
 
 public abstract class AbstractServiceTest {
-    protected static final Logger log = LogManager.getRootLogger();
+	protected static final Logger log = LogManager.getRootLogger();
 
-    @BeforeClass // run once before each test class
-    public static void setUpBeforeAll() throws Exception {
-        // run tests with a clean database!!!
-        Main.cleanup();
-    }
+	@BeforeClass // run once before each test class
+	public static void setUpBeforeAll() throws Exception {
+		CleanupService service = new CleanupService();
+		service.execute();
+	}
 
-    @Before // run before each test
-    public void setUp() throws Exception {
-        try {
-            FenixFramework.getTransactionManager().begin(false);
-            populate();
-        } catch (WriteOnReadError | NotSupportedException | SystemException e1) {
-            e1.printStackTrace();
-        }
-    }
 
-    @After // rollback after each test
-    public void tearDown() {
-        try {
-            FenixFramework.getTransactionManager().rollback();
-        } catch (IllegalStateException | SecurityException | SystemException e) {
-            e.printStackTrace();
-        }
-    }
+	@Before // run before each test
+	public void setUp() throws Exception {
+		try {
+			FenixFramework.getTransactionManager().begin(false);
+			populate();
+		} catch (WriteOnReadError | NotSupportedException | SystemException e1) {
+			e1.printStackTrace();
+		}
+	}
 
-    protected abstract void populate(); // each test adds its own data
+	@After // rollback after each test
+	public void tearDown() {
+		try {
+			FenixFramework.getTransactionManager().rollback();
+		} catch (IllegalStateException | SecurityException | SystemException e) {
+			e.printStackTrace();
+		}
+	}
+
+	protected abstract void populate(); // each test adds its own data
 }
