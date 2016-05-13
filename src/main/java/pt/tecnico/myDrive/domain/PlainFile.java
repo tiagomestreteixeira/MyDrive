5c18a115fd7b0c874060132a2661774588ffb52a
tiagomestreteixeira <tam.teixeira@gmail.com>
2016-05-13 11:54:10 +0100

    Added Execute-file root verification

--- src/main/java/pt/tecnico/myDrive/domain/PlainFile.java
+++ src/main/java/pt/tecnico/myDrive/domain/PlainFile.java
@@ -47,6 +47,17 @@ public class PlainFile extends PlainFile_Base {
     }
 
     @Override
+    public void execute(User user, String[] args) {
+        if (user.checkPermission(this, 'r')) {
+            this.getContent().split("\n");
+            System.out.println(this.getContent());//TODO: dummy
+        } else {
+            throw new NoPermissionException("PlainFile.read()");
+        }
+    }
+
+
+    @Override
     public void write(User user, String string) {
         if (user.checkPermission(this, 'w')) {
             this.setContent(string);
--- src/main/java/pt/tecnico/myDrive/service/ExecuteFileService.java
+++ src/main/java/pt/tecnico/myDrive/service/ExecuteFileService.java
@@ -6,24 +6,35 @@ import pt.tecnico.myDrive.exception.InvalidFileTypeException;
 import pt.tecnico.myDrive.exception.MyDriveException;
 
 public class ExecuteFileService extends MyDriveService {
-	private long token;
-	private String pathname;
-	private String[] args;
+    private long token;
+    private String pathname;
+    private String[] args;
 
-	public ExecuteFileService(long token, String pathname, String[] args) {
-		this.token = token;
-		this.pathname = pathname;
-		this.args = args;
-	}
-	
-	@Override
-	protected void dispatch() throws MyDriveException {
-		MyDrive md = MyDrive.getInstance();
-		Login login = md.getLoginFromId(token);
-		login.refreshToken();
-		User user = login.getUser();
-		File file = user.lookup(pathname);
-		file.execute(user,args);
-	}
+    public ExecuteFileService(long token, String pathname, String[] args) {
+        this.token = token;
+        this.pathname = pathname;
+        this.args = args;
+    }
+
+    @Override
+    protected void dispatch() throws MyDriveException {
+        MyDrive md = MyDrive.getInstance();
+
+
+        Login login = md.getLoginFromId(token);
+        login.refreshToken();
+        User user = login.getUser();
+        Dir currentDir = login.getCurrentDir();
+        if (!pathname.startsWith("/")) {
+            if (currentDir.getPath().equals("/")) {
+                pathname = currentDir.getPath() + pathname;
+
+            } else {
+                pathname = currentDir.getPath() + "/" + pathname;
+            }
+        }
+        File file = user.lookup(pathname);
+        file.execute(user, args);
+    }
 }
 
